bc816e8590380af7b2057549e147d8181cf39593
Combome <1065074032@qq.com>
2019-05-18 15:00:21 +0800

    新建工程

---
+++ WeChatMoment/WeChatMoment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeChatMoment
+//
+//  Created by max on 2019/5/18.
+//  Copyright © 2019 max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
